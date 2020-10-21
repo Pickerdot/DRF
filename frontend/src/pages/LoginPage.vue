4ffb3b5ca595c5a6853d661b1b6313e1a3c80c43
picker <main1@pickerlab.net>
2020-10-21 09:53:30 +0900

    交代

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="login-page">
+    <GlobalHeader/>
+    <GlobalMessage/>
+
+    <!--メインアリア-->
+    <main class="container">
+        <p class="h5 mb-4">ログイン</p>
+        <b-form @dubmit.prevent="submitLogin">
+            <div class="row form-group">
+
+            </div>
+            </b-form>
+    
+</template>
